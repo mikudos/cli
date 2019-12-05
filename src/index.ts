81a6f090060ef8c504161027ed311bf6457d4888
guanyu <yue.guanyu@hotmail.com>
2019-12-06 00:24:09 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import commander from 'commander';
+import yeoman from 'yeoman-environment';
+import semver from 'semver';
+import updateNotifier from 'update-notifier';
+
+const env = yeoman.createEnv();
+
+export default function(argv: string[], generatorOptions = {}) {
+    const pkg = require('../package.json');
+    updateNotifier({ pkg }).notify();
+
+    commander
+        .version(pkg.version)
+        .usage('upgrade <version>')
+        .usage('generate [type]');
+
+    if (!semver.satisfies(process.version, '>= 12.0.0')) {
+        console.error(
+            'The Mikudos CLI and generated application requires Node v12.0.0 or later.'
+        );
+        return process.exit(1);
+    }
+    console.log('TCL: argv', argv);
+}
